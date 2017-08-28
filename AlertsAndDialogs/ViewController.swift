aa807a1237e59f2c9d7266d04c8e5044cd7af088
katie <katie@Katies-MacBook-Pro.local>
2017-08-28 13:39:16 +0300

    Initial Commit

---
+++ AlertsAndDialogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertsAndDialogs
+//
+//  Created by Boris on 28/08/2017.
+//  Copyright © 2017 dolyaboris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
